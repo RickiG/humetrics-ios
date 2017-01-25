b49e3423703e27f2e357d2301488eb88388dfa3b
Ricki Gregersen <hi@rickigregersen.com>
2017-01-25 20:17:47 +0100

    Initial Commit

---
+++ Humetrics-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humetrics-ios
+//
+//  Created by Ricki Gregersen on 25/01/2017.
+//  Copyright © 2017 rickigregersen.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
